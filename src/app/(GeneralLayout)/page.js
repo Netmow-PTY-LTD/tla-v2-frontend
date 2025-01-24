bab3369c115eae077697e36b208a71889135fa9b
tuhin.netmow <tuhin.netmow@gmail.com>
2025-01-24 17:39:21 +0600

    TestCounter removed

--- src/app/(GeneralLayout)/page.js
+++ src/app/(GeneralLayout)/page.js
@@ -1,15 +1,9 @@
-import TestCounter from "@/components/UI/TestCounter";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-2xl sm:text-3xl font-bold text-center sm:text-left">
         Welcome to The LawAppOnline
       </h1>
-
-      <div>
-        <TestCounter />
-      </div>
     </div>
   );
 }
