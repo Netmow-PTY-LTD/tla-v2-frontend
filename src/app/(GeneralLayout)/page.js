89e46e79c21bb10d47557c2e7fda63d1b05d55c4
rabby.dev <rabby.netmow@gmail.com>
2025-01-24 14:41:42 +0600

    rearchitect style folder

--- src/app/(GeneralLayout)/page.js
+++ src/app/(GeneralLayout)/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-2xl sm:text-3xl text-red-600 font-bold text-center sm:text-left">
+      <h1 className="text-2xl sm:text-3xl font-bold text-center sm:text-left text-red-500">
         Get started with Next.js
       </h1>
     </div>
