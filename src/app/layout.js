735ce4e022d73ee669c1c2600fe1fcc4db04fd2e
rabby.dev <rabby.netmow@gmail.com>
2025-01-24 15:35:16 +0600

    rename redux provider file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
-import ReduxProviderWrapper from "@/lib/providers/ReduxProvider.tsx";
+import ReduxProviderWrapper from "@/lib/providers/ReduxProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
